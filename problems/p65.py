97e0b00e44f0249751f251c6b05213886474352e
Alex Spencer <aspencer@rmplc.net>
2022-05-24 11:44:35 +0100

    Add p65

---
+++ problems/p65.py
@@ -0,0 +1,12 @@
+# start with numerator as 2
+# Calculating the 3rd numerator
+
+desired_nth_convergent_numerator = 100
+
+numerator = 2
+previous_numerator = 1
+for i in range(2, desired_nth_convergent_numerator+1):
+    multiplier = 2 * int(i / 3) if i % 3 == 0 else 1
+    previous_numerator, numerator = numerator, previous_numerator + (numerator * multiplier)  # Nifty, thanks python
+
+print(sum(map(int, str(numerator)))) # Also nice
