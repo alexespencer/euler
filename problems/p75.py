2d874784a4d6c8615253de6c7285468a875a3e5b
Alex Spencer <alex.spencer@alpharodtech.co.uk>
2022-05-30 08:52:13 +0100

    P75 WIP

--- problems/p75.py
+++ problems/p75.py
@@ -5,32 +5,11 @@ from euler import is_square
 
 triangles = {}
 
-# The area of a right angled triangles with integer sides is always divisible by 3 
+# For a right angled triangle with integer sides there are a number of properties we might be able to use:
+# a + b + c % 6 == 9
+# a * b * c % 60 == 0
+# We could try iterating x 60, 120, 180 etc - and finding triangles that have a * b * c = x
 
 max_length = 150000
-for a in range(3, max_length // 3):
-    if a % 10000 == 0:
-        print(a)
-    
-    for b in range(a+1, max_length // 3):
-        if (a * b) / 2 % 3 == 0:
-            # Determine length
-            c = (a ** 2 + b ** 2)
-
-            if not is_square(c):
-                continue
-
-            length = int(a + b + (c ** 0.5))
-            if length > max_length:
-                break
-
-            triangles.setdefault(length, [])
-            if (a, b, c) not in triangles[length]:
-                triangles[length].append((a, b, c))
-
-count = 0
-for length, triangles in triangles.items():
-    if len(triangles) == 1 and length <= max_length:
-        count += 1
-        # print(length, triangles)
-print(count)
+for x in range(60, 1000, 60):
+    pass
