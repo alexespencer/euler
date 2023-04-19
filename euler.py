48a7927ad864e2123a8c27c508f2ad37a1458d90
Alex Spencer <alex.spencer@alpharodtech.co.uk>
2023-04-19 16:28:48 +0100

    Improve prime performance

--- euler.py
+++ euler.py
@@ -59,7 +59,10 @@ def is_prime(n):
     if n == 2:
         return True
 
-    for i in range(2, int(sqrt(n+1)+1)):
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, int(sqrt(n+1)+1), 2):
     # for i in range(2, n):
         if n % i == 0:
             return False
