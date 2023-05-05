a585200208031ab4f0776573691c4aa5c3913cc1
Alex Spencer <alex.spencer@alpharodtech.co.uk>
2023-05-05 15:19:26 +0100

    Tidy up

--- problems/p57.py
+++ problems/p57.py
@@ -1,23 +1,20 @@
 # Square root convergents
-
-from lib2to3.refactor import get_all_fix_names
-
-
 def get_fraction_parts(n):
     """
     Returns the numerator and demoninator of a fraction for the nth expansion
     """
-    a, b = 1, 2
+    numerator , denominator = 1, 2
 
     # Counter for the number of times the numerator has more digits than the demoninator
     count = 0
 
-    for _ in range(n-1):
-        a, b = b, (2 * b + a) # Funky little swap - thanks python
-        if len(str(a+b)) > len(str(b)): # Don't forget to add the demoninator to the numerator for the "+1" part
+    for i in range(1, n):
+        numerator, denominator = denominator, (2 * denominator + numerator ) # Funky little swap - thanks python
+        if len(str(numerator +denominator)) > len(str(denominator)): # Don't forget to add the demoninator to the numerator for the "+1" part
+            print(i+1, numerator+denominator, denominator)
             count += 1
-    
-    return a, b, count
+
+    return numerator , denominator, count
 
 assert get_fraction_parts(1) == (1, 2, 0)
 assert get_fraction_parts(2) == (2, 5, 0)
@@ -28,5 +25,6 @@ assert get_fraction_parts(6) == (239-169, 169, 0)
 assert get_fraction_parts(7) == (577-408, 408, 0)
 assert get_fraction_parts(8) == (1393-985, 985, 1)
 
-a, b, count = get_fraction_parts(1000)
+print("----")
+a, b, count = get_fraction_parts(13)
 print(count)
