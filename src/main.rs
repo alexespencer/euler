8c8f5953b6e3d3cf0ee8eb1a96ec2775917482a1
Alex Spencer <alex.spencer@helsing.ai>
2024-03-07 11:11:20 +0000

    Add p1

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,18 @@ fn fibonacci() -> Fibonacci {
 }
 
 fn main() {
+    // Problem 1 - Fizz buzz
+    let p1: u64 = (1_u64..1000_u64)
+        .filter(|&x| x.rem_euclid(3) == 0 || x.rem_euclid(5) == 0)
+        .sum();
+
+    println!("Problem 1 answer: {}", p1);
+    assert_eq!(233168, p1);
+
+    // Problem 2 - fibonacci
     for x in fibonacci().take(10) {
         println!("{}", x);
     }
-
     let p2: u64 = fibonacci()
         .filter(|&x| x.rem_euclid(2) == 0)
         .take_while(|&x| x <= 4_000_000_u64)
