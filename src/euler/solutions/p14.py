20cd9381550cbdb9abca1b07a750386b4fb9c627
Alex Spencer <alexedwardspencer@gmail.com>
2025-12-31 16:05:04 +0000

    Tidy up p14

--- src/euler/solutions/p14.py
+++ src/euler/solutions/p14.py
@@ -5,42 +5,19 @@ def seq(n):
         return 3 * n + 1
 
 
-def longest_collatz(n):
+def longest_collatz(n, collatz_length):
     if n == 1:
         return 1
     elif n in collatz_length:
         return collatz_length[n]
     else:
-        length = 1 + longest_collatz(seq(n))
+        length = 1 + longest_collatz(seq(n), collatz_length)
         collatz_length[n] = length
 
         return length
 
 
-collatz_length = {}
-
-
-assert longest_collatz(1) == 1
-assert longest_collatz(2) == 2
-assert longest_collatz(4) == 3
-assert longest_collatz(8) == 4
-assert longest_collatz(13) == 10
-
-n_to_length_lookup = {}
-
-longest_chain = 0
-for i in range(1, 1000000):
-    length = longest_collatz(i)
-    if length >= longest_chain:
-        longest_chain = length
-        n_to_length_lookup.setdefault(i, [0, set()])
-        n_to_length_lookup[i][0] = length
-        n_to_length_lookup[i][1].add(i)
-
-print(collatz_length)
-
-
-def longest_less_n(n):
+def longest_less_n(n, n_to_length_lookup):
     # Find the largest key in n_to_length_lookup that is less than or equal to n
     key = max([k for k in n_to_length_lookup if k <= n])
 
@@ -49,8 +26,27 @@ def longest_less_n(n):
 
 
 def solution() -> int:
-    assert longest_less_n(1000000) == 837799
-    return longest_less_n(1000000)
+    collatz_length = {}
+
+    assert longest_collatz(1, collatz_length) == 1
+    assert longest_collatz(2, collatz_length) == 2
+    assert longest_collatz(4, collatz_length) == 3
+    assert longest_collatz(8, collatz_length) == 4
+    assert longest_collatz(13, collatz_length) == 10
+
+    n_to_length_lookup = {}
+
+    longest_chain = 0
+    for i in range(1, 1000000):
+        length = longest_collatz(i, collatz_length)
+        if length >= longest_chain:
+            longest_chain = length
+            n_to_length_lookup.setdefault(i, [0, set()])
+            n_to_length_lookup[i][0] = length
+            n_to_length_lookup[i][1].add(i)
+
+    assert longest_less_n(1000000, n_to_length_lookup) == 837799
+    return longest_less_n(1000000, n_to_length_lookup)
 
 
 if __name__ == "__main__":
