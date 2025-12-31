9afa13ac97628d986769b981e125ac28efce4337
Alex Spencer <alexedwardspencer@gmail.com>
2025-12-31 16:02:41 +0000

    Tidy p12

--- src/euler/solutions/p12.py
+++ src/euler/solutions/p12.py
@@ -45,47 +45,7 @@ def number_factors(n):
     return prod([ex + 1 for ex in pf.values()])
 
 
-for i in range(1, 1000):
-    assert number_factors(i) == len(find_factors(i))
-
-factor_count_lookup = {
-    0: 1,
-    1: 3,
-}  # key: n value = value of the FIRST triangle number with OVER the n (key) factors
-
-# What's the first number with 10 ** 3 divisors?
-# The first number with OVER 10**3 factors is 842161320. It is the 41040th triangle number and has 1024 factors
-# The "first numbers" with over N factors are always even (after the first 2 triangle numbers). This means we can
-# reduce the numbers we search by skipping over odd triangle numbers. The pattern is [odd, odd, even even]
-limit = 10**3
-sum = 6
-i = 3
-keep_going = True
-max_factors_found = 2
-while keep_going:
-    for _ in range(2):
-        num_factors = number_factors(sum)
-        if num_factors > max_factors_found:
-            max_factors_found = num_factors
-            key = num_factors - 1
-            factor_count_lookup[key] = sum
-
-            if num_factors > limit:
-                keep_going = False
-                break
-
-        i += 1
-        sum += i
-
-    for _ in range(2):
-        i += 1
-        sum += i
-
-for k, v in factor_count_lookup.items():
-    print(k, v)
-
-
-def first_triangle_number_with_over_n_factors(n):
+def first_triangle_number_with_over_n_factors(n, factor_count_lookup):
     # Find the first key greater than or equal to n
     key = min([k for k in factor_count_lookup.keys() if k >= n])
 
@@ -93,12 +53,50 @@ def first_triangle_number_with_over_n_factors(n):
 
 
 def solution() -> int:
-    assert first_triangle_number_with_over_n_factors(5) == 28
-    value = first_triangle_number_with_over_n_factors(500)
+    for i in range(1, 1000):
+        assert number_factors(i) == len(find_factors(i))
+
+    factor_count_lookup = {
+        0: 1,
+        1: 3,
+    }  # key: n value = value of the FIRST triangle number with OVER the n (key) factors
+
+    # What's the first number with 10 ** 3 divisors?
+    # The first number with OVER 10**3 factors is 842161320. It is the 41040th triangle number and has 1024 factors
+    # The "first numbers" with over N factors are always even (after the first 2 triangle numbers). This means we can
+    # reduce the numbers we search by skipping over odd triangle numbers. The pattern is [odd, odd, even even]
+    limit = 10**3
+    sum = 6
+    i = 3
+    keep_going = True
+    max_factors_found = 2
+    while keep_going:
+        for _ in range(2):
+            num_factors = number_factors(sum)
+            if num_factors > max_factors_found:
+                max_factors_found = num_factors
+                key = num_factors - 1
+                factor_count_lookup[key] = sum
+
+                if num_factors > limit:
+                    keep_going = False
+                    break
+
+            i += 1
+            sum += i
+
+        for _ in range(2):
+            i += 1
+            sum += i
+
+    for k, v in factor_count_lookup.items():
+        print(k, v)
+
+    assert first_triangle_number_with_over_n_factors(5, factor_count_lookup) == 28
+    value = first_triangle_number_with_over_n_factors(500, factor_count_lookup)
     assert value == 76576500
     return value
 
 
 if __name__ == "__main__":
-    value = solution()
-    print(value)
+    print(solution())
