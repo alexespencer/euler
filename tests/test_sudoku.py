3c831ca8ac77eeeea10b5d0ab10e8bf07eb609ab
Alex Spencer <alexedwardspencer@gmail.com>
2026-01-01 10:09:59 +0000

    Remove commented code

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -198,46 +198,6 @@ class TestSudoku:
         assert 1 not in s.possible_values[8][8]
         assert 9 not in s.possible_values[8][8]
 
-    # def test_find_naked_pairs_box(self):
-    #     # Need to manually disable row/col fix for now
-    #     s = Sudoku(['000000000', '967345820', '251876403', '548032976', '729564138', '136708245', '372680514', '814253760', '695007382'])
-    #     print(s)
-    #     # assert False is True
-    #     # Force a possible 1
-    #     s.possible_values[2][3].append(1)
-    #     assert 1 in s.possible_values[2][3]
-
-    #     print(s.find_naked_pairs())
-    #     assert 1 not in s.possible_values[2][3]
-
-    # def test_find_hidden_n(self):
-    # TODO: Find a soduku that this test works on
-    #     s = Sudoku(['000003017','015009038','063000002','100007000','009006200','000502004','000000020','500600340','340200000'])
-
-    #     # Run solve without hidden, then assert it removes possible values
-    #     updated = True
-    #     while updated:
-    #         updated = False
-    #         updated = s.find_single() or updated
-    #         updated = s.find_hidden_single() or updated
-    #         updated = s.find_locked_candidates() or updated
-    #         updated = s.find_naked_pairs() or updated
-
-    #     assert s.solved() == False
-    #     print(s.possible_values)
-    #     print(s.possible_values[0][4])
-    #     assert 2 in s.possible_values[0][4]
-    #     assert 8 in s.possible_values[0][4]
-    #     assert 4 in s.possible_values[0][6]
-    #     assert 9 in s.possible_values[0][6]
-
-    #     assert s.find_hidden_n() == True
-
-    #     assert 2 not in s.possible_values[0][4]
-    #     assert 8 not in s.possible_values[0][4]
-    #     assert 4 not in s.possible_values[0][6]
-    #     assert 9 not in s.possible_values[0][6]
-
     def test_column_possible_values(self):
         s = Sudoku(
             [
